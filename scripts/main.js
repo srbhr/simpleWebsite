96ab82d7d15bf71e37e2a22e9ab7b7ad675ef8ef
srbhr <srbh077@gmail.com>
2022-03-02 23:15:55 +0530

    Website Improvements

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,35 @@
-const myHeading = document.querySelector('h1');
+let myImg = document.querySelector('img');
 
-myHeading.textContent = "Hello World!";
+myImg.onclick = function () {
+    let mySrc = myImg.getAttribute('src');
+
+    if (mySrc == "assets/Firefox-logo.png") {
+        myImg.setAttribute("src", "assets/Firefox-logo-2.png");
+    } else {
+        myImg.setAttribute("src", "assets/Firefox-logo.png");
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
